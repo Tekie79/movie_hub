5784968f53448a318470ab25ac8d1fff6f8674e9
Tekie79 <tekiefilm79@gmail.com>
2021-07-10 20:08:12 -0400

    Trending Page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Header.css';
-import MovieFilterOutlinedIcon from '@material-ui/icons/MovieFilterOutlined';
+import React from "react";
+import "./Header.css";
+import MovieFilterOutlinedIcon from "@material-ui/icons/MovieFilterOutlined";
 
 const Header = () => {
-    return (
-        <span className="header">
-             Movie Hub
-             <MovieFilterOutlinedIcon fontSize="large" />
-             </span>
-    )
-}
+  return (
+    <span className="header" onClick={() => window.scroll(0, 0)}>
+      Movie Hub
+      <MovieFilterOutlinedIcon fontSize="large" />
+    </span>
+  );
+};
 
-export default Header
+export default Header;
