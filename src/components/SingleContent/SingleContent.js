53894592a4f57586acf621b4e4ec6e035f87630a
Tekie79 <tekiefilm79@gmail.com>
2023-02-28 23:33:18 -0500

    rating change

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -15,22 +15,21 @@ const SingleContent = ({
   const year = new Date(date);
   return (
     <ContentModal media_type={media_type} id={id}>
-    <Badge
-      badgeContent={vote_average}
-      color={vote_average > 6 ? "primary" : "secondary"}
-    />
-    <img
-      className="poster"
-      src={poster ? `${img_300}/${poster}` : unavailable}
-      alt={title}
-    />
-    <b className="title">{title}</b>
-    <span className="subtitle">
-      {media_type === "tv" ? "TV Series" : "Movie" }
-      <span className="subtitle">{year.getFullYear()}</span>
-    </span>
-  </ContentModal>
-    
+      <Badge
+        badgeContent={vote_average.toFixed(1)}
+        color={vote_average > 6 ? "primary" : "secondary"}
+      />
+      <img
+        className="poster"
+        src={poster ? `${img_300}/${poster}` : unavailable}
+        alt={title}
+      />
+      <b className="title">{title}</b>
+      <span className="subtitle">
+        {media_type === "tv" ? "TV Series" : "Movie"}
+        <span className="subtitle">{year.getFullYear()}</span>
+      </span>
+    </ContentModal>
   );
 };
 
